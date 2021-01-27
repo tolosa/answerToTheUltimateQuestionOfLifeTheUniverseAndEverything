3f6a2ee07f2010d7eeaa4d869fed7f4be700653a
Leonardo Tolosa <tolosa@gmail.com>
2021-01-27 18:20:34 -0300

    Add basic code

---
+++ index.js
@@ -0,0 +1,13 @@
+const answerToTheUltimateQuestionOfLifeTheUniverseAndEverything = 42
+
+const isTheAnswerToTheUltimateQuestionOfLifeTheUniverseAndEverything =
+  (value) => value == answerToTheUltimateQuestionOfLifeTheUniverseAndEverything
+
+const includesTheAnswerToTheUltimateQuestionOfLifeTheUniverseAndEverything =
+  (value) => String(value).includes(answerToTheUltimateQuestionOfLifeTheUniverseAndEverything)
+
+module.exports = {
+  answerToTheUltimateQuestionOfLifeTheUniverseAndEverything,
+  isTheAnswerToTheUltimateQuestionOfLifeTheUniverseAndEverything,
+  includesTheAnswerToTheUltimateQuestionOfLifeTheUniverseAndEverything
+}
